077e4fa00535fdc125dd56f6340ca69286ae0e9c
Tanbir Jishan <tantalising007@gmail.com>
2024-08-03 00:26:41 +0530

    Changed the slot store so that the slot set can be modified while being iterated over. This will help when a slot connects or disconnects itself or other slots in a signal emission.

--- lib/src/slot_store.dart
+++ lib/src/slot_store.dart
@@ -1,14 +1,36 @@
 import 'dart:collection';
 
+/// This class wraps a hashSet to prevent direct access to it.
+///
+/// It only exposes the [SlotStore.add] and [SlotStore.remove] methods.
+/// The add and remove methods keep a list of slots to be added or removed and
+/// does not really add them to the set or remove them from the set until the
+/// [SlotStore.sync] method is called. It facilitates mutating the set while
+/// or other slots iterating over it. The intended use case is slots removing
+/// or adding itself in a signal emission. Since the underlying set is hidden,
+/// a readonly iterator of the set is provided by the [SlotStore.slots] method.
 class SlotStore {
   final HashSet<Function> _slots = HashSet();
-  add(Function slot) {
-    return _slots.add(slot);
+
+  final  _slotsToBeAdded = HashSet<Function>();
+  final _slotsToBeRemoved = HashSet<Function>();
+
+  void add(Function slot) {
+    _slotsToBeAdded.add(slot);
   }
-  remove(Function slot) {
-    return _slots.remove(slot);
+
+  void remove(Function slot) {
+    _slotsToBeRemoved.add(slot);
   }
+
   Iterable<Function> slots() {
     return List<Function>.unmodifiable(_slots);
+  }
+
+  void sync() {
+    _slots.addAll(_slotsToBeAdded);
+    _slots.removeAll(_slotsToBeRemoved);
+    _slotsToBeAdded.clear();
+    _slotsToBeRemoved.clear();
   }
 }
