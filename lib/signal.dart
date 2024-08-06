8de642094f962764212e80b67fe069f6938ff260
Tanbir Jishan <tantalising007@gmail.com>
2024-08-06 16:02:00 +0530

    todo done: library level doc.

--- lib/signal.dart
+++ lib/signal.dart
@@ -1,3 +1,7 @@
+/// flutter_signal helps you to establish communication between two
+/// entities. It provides helper widgets and classes as well for easier
+/// use with signals for state management.
+
 library flutter_signal;
 
 export 'package:flutter_signal/src/signal.dart';
@@ -5,5 +9,3 @@ export 'package:flutter_signal/src/signal_model.dart';
 export 'package:flutter_signal/src/signal_widget.dart';
 export 'package:flutter_signal/src/property.dart';
 export 'package:flutter_signal/src/property_widget.dart';
-
-//todo: library level docs
