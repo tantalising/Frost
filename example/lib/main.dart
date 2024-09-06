31d97f95626e683a72bea463a5e8b0cf67a1b380
Tanbir Jishan <tantalising007@gmail.com>
2024-09-06 14:00:06 +0530

    Add Example

---
+++ example/lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_signal/signal_model.dart';
+import 'package:flutter_signal/signal_widget.dart';
+import 'model.dart';
+
+Slot doSomething() {
+  print('doing something');
+}
+
+void main() {
+  connect(CountModel.countChanged, doSomething);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            SignalWidget(
+              signal: CountModel.countChanged,
+              model: CountModel(),
+              builder: () => Text(
+                ModelStore.get<CountModel>()!.count.toString(),
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => ModelStore.get<CountModel>()?.incrementCount(),
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+
+
+
---
+++ example/lib/model.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_signal/signal.dart';
+import 'package:flutter_signal/signal_model.dart';
+
+class CountModel extends SignalModel {
+  static final countChanged = Signal();
+  var _count = 0;
+
+  int get count => _count;
+  void incrementCount() {
+    _count++;
+  }
+
+  @override
+  void init() {
+    // init something here
+    super.init();
+  }
+
+  @override
+  void dispose() {
+    // dispose something here.
+    super.dispose();
+  }
+}
+
+
